2f1f2dadd7c71a065545467923590c140891b3a8
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-08-19 03:17:32 +0300

    updates homepage in packege.json

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/project-healthy-hub">
+          <BrowserRouter basename="/app-healthy-hub">
             <App />
           </BrowserRouter>
         </PersistGate>
